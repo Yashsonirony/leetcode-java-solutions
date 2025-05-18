e01a042f1ddd1e51de761234eee772263c05637e
Yash Soni <158543667+Yashsonirony@users.noreply.github.com>
2025-05-18 23:05:38 +0530

    Add files via upload
    done

--- LargestRectangleArea.java
+++ LargestRectangleArea.java
@@ -1,40 +1,44 @@
-package Algorithms.algorithm.others;
+package Algorithms.array;
+
 import java.util.Stack;
 
-
 public class LargestRectangleArea {
-    public int largestRectangleArea(int[] height) {
-
+	public static void main(String[] strs) {
+		int[] height = {0};
+		System.out.println(largestRectangleArea(height));
+	}
+	
+	public static int largestRectangleArea(int[] height) {
         if (height == null || height.length == 0) {
-
-        return 0;
-
+            return 0;
         }
-
-        Stack<Integer> stack = new Stack<Integer>();
-
+        
+        Stack<Integer> s = new Stack<Integer>();
+        
         int max = 0;
-
-        for (int i = 0; i <= height.length; i++) {
-
-        int curt = (i == height.length) ? -1 : height[i];
-
-        while (!stack.isEmpty() && curt <= height[stack.peek()]) {
-
-        int h = height[stack.pop()];
-
-        int w = stack.isEmpty() ? i : i - stack.peek() - 1;
-
-        max = Math.max(max, h * w);
-
+        
+        int len = height.length;
+        int i = 0;
+        
+        while (i <= len) {
+            // BUG 1: should use height[s.peek()] instead of s.peek().
+            // BUG 2: should put i < length after the s.isEmpty.
+            // The last step: Length is also put into the stack and will break at last.
+            if (s.isEmpty() || (i < len && height[i] >= height[s.peek()])) {
+                s.push(i);
+                i++;
+            // Keep a Ascending sequence in the stack.    
+            } else {
+                // Stack is not empty, and the current node is smaller than the one in the stack.
+                // When we come to the end of the array, we will also should count all the solutions.
+                // BUG 3: should use height[s.pop] instead of s.pop
+                // When the i come to the end, the rectangle will be counted again.
+                int h = height[s.pop()];
+                int width = s.isEmpty() ? i: i - s.peek() - 1;
+                max = Math.max(max, h * width);
+            } 
         }
-
-        stack.push(i);
-
-        }
-
+        
         return max;
-
-        }
-
+    }
 }
