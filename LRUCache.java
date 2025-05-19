cbd9c479ffc737c2b43d462f3524b659a10e72db
Yash Soni <158543667+Yashsonirony@users.noreply.github.com>
2025-05-19 10:29:06 +0530

    Add files via upload
    done

--- LRUCache.java
+++ LRUCache.java
@@ -1,101 +1,90 @@
-package Algorithms.algorithm.others;
+package Algorithms.hash;
 
 import java.util.HashMap;
 
-public class LRUCache{
-    public static void main(String[] strs) {
-        LRUCache lru = new LRUCache(1);
-        lru.set(2,1);
-        lru.get(2);
-        System.out.print("xx");
-    }
-    
-    private HashMap<Integer, Node> map;
-    private int capacity;
-    private Node head;
-    private Node tail;
-        
-    private class Node {
-        Node pre;
-        Node next;
+public class LRUCache {
+    private class DLink {
+        DLink pre;
+        DLink next;
+        int val;
         int key;
-        int value;
-        
-        public Node (int key, int value) {
+        DLink(int key, int val) {
+            this.val = val;
             this.key = key;
-            this.value = value;
-            this.pre = null;
-            this.next = null;
+            pre = null;
+            next = null;
         }
     }
     
-    private void removeFirst() {
-        head.next = head.next.next;
-        head.next.pre = head;
+    HashMap<Integer, DLink> map;
+    
+    DLink head;
+    DLink tail;
+    
+    int capacity;
+
+    public void removeFist() {
+        removeNode(head.next);
     }
     
-    public LRUCache(int capacity) {
-        this.capacity = capacity;
-        map = new HashMap<Integer, Node>();
-        
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
-        
-        head.next = tail;
-        tail.pre = head;
-    }
-    
-    public int get(int key) {
-        if (!map.containsKey(key)) {
-            return -1;
-        }
-        
-        Node node = map.get(key);
-        
-        // link the pre to the next. link the next to the pre.
-        removeNode(node);
-        
-        // add the node to the tail.
-        addToTail(node);
-        
-        return map.get(key).value;
-    }
-    
-    public void removeNode(Node node) {
+    public void removeNode(DLink node) {
         node.pre.next = node.next;
         node.next.pre = node.pre;
     }
     
-    public void addToTail(Node node) {
+    // add a node to the tail.
+    public void addToTail(DLink node) {
         tail.pre.next = node;
-        node.next = tail;
         
         node.pre = tail.pre;
+        node.next = tail;
+        
         tail.pre = node;
     }
     
-    public void set(int key, int value) {
+    public LRUCache(int capacity) {
+        map = new HashMap<Integer, DLink>();
         
+        // two dummy nodes. In that case, we can deal with them more conviencely.
+        head = new DLink(-1, -1);
+        tail = new DLink(-1, -1);
+        head.next = tail;
+        tail.pre = head;
         
-        Node insert;
-        if (map.containsKey(key)) {
-            insert = map.get(key);
-            insert.value = value;
-            
-            // move out the insert from the list.
-            removeNode(insert);
-        } else {
-            // remove a element if the size is full.        
-            if (capacity == map.size()) {
-                map.remove(head.next.key);
-                removeFirst();            
-            }
-            
-            insert = new Node(key, value);
-            map.put(key, insert);
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        if (map.get(key) == null) {
+            return -1;
         }
         
-        // move the node to the tail.
-        addToTail(insert);
+        // update the node.
+        DLink node = map.get(key);
+        removeNode(node);
+        addToTail(node);
+        
+        return node.val;
+    }
+    
+    public void set(int key, int value) {
+        DLink node = map.get(key);
+        if (node == null) {
+            // create a node and add the key-node pair into the map.
+            node = new DLink(key, value);
+            map.put(key, node);
+        } else {
+            // update the value of the node.
+            node.val = value;
+            removeNode(node);
+        }
+        
+        addToTail(node);
+        
+        // if the LRU is full, just remove a node.
+        if (map.size() > capacity) {
+            map.remove(head.next.key);
+            removeFist();
+        }
     }
 }
