f384971b9ff2002ba9fd421d2160533996b85a49
Yash Soni <158543667+Yashsonirony@users.noreply.github.com>
2025-05-19 08:13:20 +0530

    Add files via upload
    done

--- CombinationSum.java
+++ CombinationSum.java
@@ -1,78 +1,42 @@
-package Algorithms.algorithm.others;
+package Algorithms.combination;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class CombinationSum{
-    public ArrayList<ArrayList<Integer>> combine(int n, int k) {
-        ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>>();
-        
-        help(n, 0, new ArrayList<Integer>(), rst, k);
-        
-        return rst;
-    }
-    
-    public void help(int n, int index, ArrayList<Integer> path, ArrayList<ArrayList<Integer>> rst, int k) {
-        if (path.size() == k) {
-            rst.add(new ArrayList<Integer>(path));
-            return;
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        if (candidates == null || candidates.length == 0) {
+            return ret;
         }
         
-        for (int i = index; i < n; i++) {
-            path.add(i + 1);
-            help(n, i + 1, path, rst, k);
-            path.remove(path.size() - 1);
-        }
+        List<Integer> path = new ArrayList<Integer>();
         
-        return;
-    }
-    
-    
-    public static void main(String[] args) {
-        CombinationSum cs = new CombinationSum();
-        
-        int[] num = {1,2};
-        cs.combine(1, 1);
-        
-        
-        
-        
-        
-    }
-    
-    public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
-        ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>> ();
+        // we should sort the candidates than do it. in this case we can get a non-descending order.
         Arrays.sort(candidates);
-
-        help(candidates, 0, new ArrayList<Integer>(), rst, target);
         
-        System.out.printf(rst.toString());
-        
-        return rst;
+        combinationSum(candidates, target, path, ret, 0);
+        return ret;
     }
     
-    public void help(int[] cand, int index, ArrayList<Integer> path, ArrayList<ArrayList<Integer>> rst, int target) {
+    public void combinationSum(int[] candidates, int target, List<Integer> path, List<List<Integer>> ret, int index) {
         if (target == 0) {
             // add the current set into the result.
-            rst.add(new ArrayList<Integer>(path));
+            ret.add(new ArrayList<Integer>(path));
             return;
         }
         
-        int pre = -1;
-        for (int i = index; i < cand.length; i++) {
-            if (cand[i] > target) {
-                // because the sequence is ascending, so we don't need to go on.
-                break;
-            }
-            
-            if (cand[i] == pre)
-            
-            path.add(cand[i]);
-            help(cand, index, path, rst, target - cand[i]);
-            path.remove(path.size() - 1);
+        if (target < 0) {
+            return;
         }
         
-        return;
+        int len = candidates.length;
+        for (int i = index; i < len; i++) {
+            int num = candidates[i];
+            path.add(num);
+            combinationSum(candidates, target - num, path, ret, i);
+            path.remove(path.size() - 1);
+        }
     }
-
 }
