6a729e27f2453689b270babcd1691ac6b6e6798d
Yash Soni <158543667+Yashsonirony@users.noreply.github.com>
2025-05-22 22:22:50 +0530

    Add files via upload
    done

--- Partition.java
+++ Partition.java
@@ -1,125 +1,57 @@
-package Algorithms.dfs;
+package Algorithms.list;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import Algorithms.algorithm.others.ListNode;
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Partition {
-    public List<List<String>> partition1(String s) {
-        List<List<String>> ret = new ArrayList<List<String>>();
-        List<String> path = new ArrayList<String>();
-
-        if (s == null) {
-            return ret;
+    public ListNode partition(ListNode head, int x) {
+        if (head == null) {
+            return null;
         }
-
-        HashMap<String, Boolean> map = new HashMap<String, Boolean>();
-
-        dfs(s, path, ret, 0, map);
-
-        return ret;
-    }
-
-    public boolean isPalindrom(String s) {
-        int len = s.length();
-        if (len <= 1) {
-            return true;
-        }
-
-        int left = 0;
-        int right = len - 1;
-        for (; left < right; left++, right--) {
-            if (s.charAt(right) != s.charAt(left)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-    
-    /*
-      we use a map to store the solutions to reduce the times of computing.
-    */
-    public void dfs(String s, List<String> path, List<List<String>> ret, int index, HashMap<String, Boolean> map) {
-        if (index == s.length()) {
-            ret.add(new ArrayList<String>(path));
-            return;
-        }
-
-        for (int i = index; i < s.length(); i++) {
-            String sub = s.substring(index, i + 1);
-
-            Boolean flag = map.get(sub);
-            if (flag == null) {
-                flag = isPalindrom(sub);
-                map.put(sub, flag);
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode pre = dummy;
+        ListNode cur = head;
+        
+        // Record the big list.
+        ListNode bigDummy = new ListNode(0);
+        ListNode bigTail = bigDummy;
+        
+        while (cur != null) {
+            if (cur.val >= x) {
+                // Unlink the cur;
+                pre.next = cur.next;
+               
+                // Add the cur to the tail of the new link.
+                bigTail.next = cur;
+                cur.next = null;
+               
+                // Refresh the bigTail.
+                bigTail = cur;
+               
+                // 移除了一个元素的时候，pre不需要修改，因为cur已经移动到下一个位置了。
+            } else {
+                pre = pre.next;
             }
             
-            if (!flag) {
-                continue;
-            }
-
-            path.add(sub);
-            dfs(s, path, ret, i + 1, map);
-            path.remove(path.size() - 1);
+            cur = pre.next;
         }
-    }
-
-    public List<List<String>> partition(String s) {
-        List<List<String>> ret = new ArrayList<List<String>>();
-        List<String> path = new ArrayList<String>();
-
-        if (s == null) {
-            return ret;
-        }
-
-        boolean[][] isPalindrom = buildPalindromDP(s);
-
-        dfs2(s, path, ret, 0, isPalindrom);
-
-        return ret;
-    }
-    
-    /*
-     * Solution 2: Use DP to reduce the duplicate count.
-     * */
-    boolean[][] buildPalindromDP(String s) {
-        int len = s.length();
-        boolean[][] D = new boolean[len][len];
-
-        for (int j = 0; j < len; j++) {
-        	for (int i = 0; i <= j; i++) {
-        		if (j == 0) {
-        			D[i][j] = true;
-        			continue;
-        		} 
-
-        		D[i][j] = s.charAt(i) == s.charAt(j) 
-        		    && (j - i <= 2 || D[i + 1][j - 1]);
-        	}
-        }
-
-        return D;
-    }
-
-    /*
-      we use a map to store the solutions to reduce the times of computing.
-    */
-    public void dfs2(String s, List<String> path, List<List<String>> ret, int index, boolean[][] isPalindromDP) {
-        if (index == s.length()) {
-            ret.add(new ArrayList<String>(path));
-            return;
-        }
-
-        for (int i = index; i < s.length(); i++) {
-            String sub = s.substring(index, i + 1);
-            if (!isPalindromDP[index][i]) {
-                continue;
-            }
-
-            path.add(sub);
-            dfs2(s, path, ret, i + 1, isPalindromDP);
-            path.remove(path.size() - 1);
-        }
+        
+        // Link the Big linklist to the smaller one.
+        pre.next = bigDummy.next;
+        
+        return dummy.next;
     }
 }
